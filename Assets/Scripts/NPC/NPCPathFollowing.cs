6c2f02ec51edc9e368a6a1e5f3769052c17c59c0
axel <axel.mastroianni@incloudteam.com>
2023-05-31 13:50:36 +0200

    Starting NPC movement

---
+++ Assets/Scripts/NPC/NPCPathFollowing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPathFollowing : MonoBehaviour
+{
+    [SerializeField] NPCPathType pathType;
+    [SerializeField] NPCPath npcPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
