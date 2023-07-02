29f9627501508090190e94fb7042ceefe04f7311
Axel_Mastroianni <a.mastroianni@studenti.unibs.it>
2023-07-02 22:29:57 +0200

    New progresses in the dialogue course

--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace Pokemon.Dialogue {
     public class PlayerConversant : MonoBehaviour
     {
         [SerializeField] Dialogue currentDialogue;
-        // Start is called before the first frame update
-        void Start()
-        {
-            
+        DialogueNode currentNode = null;
+
+        private void Awake() {
+            currentNode = currentDialogue.GetRootNode();
         }
 
         // Update is called once per frame
@@ -19,10 +20,25 @@ namespace Pokemon.Dialogue {
         }
 
         public string GetText() {
-            if (currentDialogue == null) {
+            if (currentNode == null) {
                 return "";
             }
-            return  currentDialogue.GetRootNode().Text;
+            return  currentNode.Text;
+        }
+
+        public IEnumerable<string> GetChoices() {
+            yield return "I've lived here for all my life";
+            yield return "I came here from Nowhere";
+        }
+
+        public void Next() {
+            DialogueNode[] children = currentDialogue.GetAllChildren(currentNode).ToArray();
+            int randomIndex = Random.Range(0, children.Count());
+            currentNode = children[randomIndex];
+        }
+
+        public bool HasNext() {
+            return currentDialogue.GetAllChildren(currentNode).Count() > 0;
         }
     }
 }
--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pokemon.Dialogue;
 using TMPro;
+using UnityEngine.UI;
 
 namespace Pokemon.UI
 {
@@ -10,17 +11,34 @@ namespace Pokemon.UI
     {
         PlayerConversant playerConversant;
         [SerializeField] TextMeshProUGUI AIText;
+        [SerializeField] Button nextButton;
+        [SerializeField] Transform choiceRoot;
+        [SerializeField] GameObject choicePrefab;
         // Start is called before the first frame update
         void Start()
         {
             playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
-            AIText.text = playerConversant.GetText();
+            nextButton.onClick.AddListener(Next);
+            UpdateUI();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            
+        private void Next() {
+
+            playerConversant.Next();
+            UpdateUI();
+        }
+
+        private void UpdateUI() {
+            AIText.text = playerConversant.GetText();
+            nextButton.gameObject.SetActive(playerConversant.HasNext());
+            foreach(Transform item in choiceRoot) {
+                Destroy(item.gameObject);
+            }
+            foreach(string choiceText in playerConversant.GetChoices()) {
+                GameObject choiceInstance = Instantiate(choicePrefab, choiceRoot);
+                var textComp = choiceInstance.GetComponentInChildren<TextMeshProUGUI>();
+                textComp.text = choiceText;
+            }
         }
     }
 }
